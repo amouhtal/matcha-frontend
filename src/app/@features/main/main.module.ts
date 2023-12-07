d4daa0f8ff447db8ed1b31818500659f99e4afe1
abderrahmane mouhtal <mouhtal.abderrahmane@gmail.com>
2023-12-07 17:01:16 +0100

    add main

---
+++ src/app/@features/main/main.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainRoutingModule } from './main-routing.module';
+import { MainComponent } from './pages/main/main.component';
+
+
+@NgModule({
+  declarations: [
+    MainComponent
+  ],
+  imports: [
+    CommonModule,
+    MainRoutingModule
+  ],
+  
+})
+export class MainModule { }
--- src/app/matcha-routing.module.ts
+++ src/app/matcha-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatComponent } from './@features/chat/pages/chat/chat.component';
+import { MainComponent } from './@features/main/pages/main/main.component';
 
 const routes: Routes = [
   { path: 'chat',  component: ChatComponent },
+  { path: 'home', component: MainComponent },
+  { path: '**', redirectTo: '/chat', pathMatch: 'full' }
 ];
 
 @NgModule({
