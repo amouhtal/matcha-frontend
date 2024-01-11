43122cefb952cc096d00d5492986f4af9f39fffc
abderrahmane mouhtal <mouhtal.abderrahmane@gmail.com>
2024-01-11 17:36:28 +0100

    add rating create ui for select rating

--- src/app/@features/browse/pages/browse/browse.component.ts
+++ src/app/@features/browse/pages/browse/browse.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 interface Profile {
   profile_avatar: string;
@@ -10,74 +10,103 @@ interface Profile {
   templateUrl: './browse.component.html',
   styleUrls: ['./browse.component.scss'],
 })
-export class BrowseComponent {
+export class BrowseComponent implements AfterViewInit {
+  rating: boolean = false;
+  rateValue: number = 0;
   profiles: Array<Profile> = [
     {
       name: 'John Doe',
-      profile_avatar: 'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
+      profile_avatar:
+        'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
       profile_location: 'London, United Kingdom',
     },
     {
       name: 'Jane Doe',
-      profile_avatar: 'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
+      profile_avatar:
+        'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
       profile_location: 'London, United Kingdom',
     },
     {
       name: 'John Doe',
-      profile_avatar: 'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
-      profile_location: 'London, United Kingdom',
-
-    },
-    {
-      name: 'Jane Doe',
-      profile_avatar: 'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
-      profile_location: 'London, United Kingdom',
-    }
-    ,
-    {
-      name: 'John Doe',
-      profile_avatar: 'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
-      profile_location: 'London, United Kingdom',
-
-    },
-    {
-      name: 'Jane Doe',
-      profile_avatar: 'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
-      profile_location: 'London, United Kingdom', 
-    },
-    {
-      name: 'John Doe',
-      profile_avatar: 'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
+      profile_avatar:
+        'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
       profile_location: 'London, United Kingdom',
     },
     {
       name: 'Jane Doe',
-      profile_avatar: 'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
+      profile_avatar:
+        'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
       profile_location: 'London, United Kingdom',
     },
     {
       name: 'John Doe',
-      profile_avatar: 'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
+      profile_avatar:
+        'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
       profile_location: 'London, United Kingdom',
-
     },
     {
       name: 'Jane Doe',
-      profile_avatar: 'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
+      profile_avatar:
+        'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
       profile_location: 'London, United Kingdom',
-    }
-    ,
+    },
     {
       name: 'John Doe',
-      profile_avatar: 'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
+      profile_avatar:
+        'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
       profile_location: 'London, United Kingdom',
-
     },
     {
       name: 'Jane Doe',
-      profile_avatar: 'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
-      profile_location: 'London, United Kingdom', 
-    }
+      profile_avatar:
+        'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
+      profile_location: 'London, United Kingdom',
+    },
+    {
+      name: 'John Doe',
+      profile_avatar:
+        'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
+      profile_location: 'London, United Kingdom',
+    },
+    {
+      name: 'Jane Doe',
+      profile_avatar:
+        'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
+      profile_location: 'London, United Kingdom',
+    },
+    {
+      name: 'John Doe',
+      profile_avatar:
+        'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
+      profile_location: 'London, United Kingdom',
+    },
+    {
+      name: 'Jane Doe',
+      profile_avatar:
+        'https://xsgames.co/randomusers/assets/avatars/pixel/31.jpg',
+      profile_location: 'London, United Kingdom',
+    },
   ];
-  constructor() {}
+  hearts!: any;
+  constructor(private starsElements: ElementRef) {}
+
+  showRating() {
+    this.rating = !this.rating;
+    if (this.rating) {
+    }
+  }
+
+  ngAfterViewInit() {
+    this.hearts = this.starsElements.nativeElement.querySelectorAll('.heart');
+  }
+  rate(index: number) {
+    this.hearts.forEach((heart: any) => {
+      heart.classList.remove('heart--active');
+    });
+    console.log('heart', this.hearts);
+    for (let i = 0; i < index; i++) {
+      this.hearts[i].classList.add('heart--active');
+    }
+    this.rateValue = index;
+  }
 }
