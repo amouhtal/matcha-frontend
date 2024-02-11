b07bbef2bd218177b95e14987caa3a62110dcc5a
abderrahmane mouhtal <mouhtal.abderrahmane@gmail.com>
2024-02-11 11:28:48 +0100

    Fix #6 : Resolve issue with chat component not displaying after merge
    the chat component was failing to display due to the JWT token being retrieved from session storage instead of local storage. This inconsistency led to unexpected behavior when making API calls to the backend, affecting the functionality of the chat component.
    
    This commit addresses the issue by updating the code to retrieve the JWT token from local storage, ensuring proper authentication and functionality of the chat component.
    
    This change resolves issue #6.

--- src/app/@api/services/chat/contacts.service.ts
+++ src/app/@api/services/chat/contacts.service.ts
@@ -8,8 +8,8 @@ export class ContactsService {
   constructor(private http: HttpClient) {}
 
   getContacts() {
-    const userID: number = sessionStorage.getItem('session')
-      ? JSON.parse(sessionStorage.getItem('session') as string).user_id
+    const userID: number = localStorage.getItem('session')
+      ? JSON.parse(localStorage.getItem('session') as string).user_id
       : 0;
     return this.http.get('http://localhost:3000/chat/conversations', {
       params: {
--- src/app/@api/services/chat/messages.service.ts
+++ src/app/@api/services/chat/messages.service.ts
@@ -8,8 +8,8 @@ export class MessagesService {
   constructor(private http: HttpClient) {}
 
   getConversationMessages(cnvId: number) {
-    const userID: number = sessionStorage.getItem('session')
-      ? JSON.parse(sessionStorage.getItem('session') as string).user_id
+    const userID: number = localStorage.getItem('session')
+      ? JSON.parse(localStorage.getItem('session') as string).user_id
       : 0;
     return this.http.get('http://localhost:3000/chat/messages', {
       params: {
